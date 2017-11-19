13cc6bcff4e6ab87efb37de52918afd6630ba887
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-11-20 00:12:25 +0100

    method to update the config sections

--- input_parser.py
+++ input_parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, re
 import xml.etree.ElementTree as ET
 
 file1 = sys.argv[1]
@@ -25,7 +25,7 @@ def recurse(node, parent_array):
 
         param_map[name] = new_path
         #if name == "extra_sun":
-        #    print("DEBUG:", name, parent_array)            
+        #    print("DEBUG:", name, parent_array)
         return 0
 
     if node.tag in ["section","conditional"]:
@@ -34,10 +34,45 @@ def recurse(node, parent_array):
 
     for subnode in node:
         recurse(subnode, parent_array)
-    
+
     return 0
 
 
 recurse(inputs, [])
-#import pdb; pdb.set_trace()
-print('\n'.join([" -- ".join([x,".".join(param_map[x])]) for x in param_map]))
+
+#print('\n'.join([" -- ".join([x,".".join(param_map[x])]) for x in param_map]))
+
+#inp = input("convert?")
+inp = "y"
+
+if inp[0].lower() == "y":
+    config_section = [x for x in root if x.tag == "configfiles"][0]
+
+    for config in config_section:
+
+        full_text = config.text
+        matching_inputs = re.findall("\$\{?[^}'\s]+\}?", full_text)
+
+        for inparam in matching_inputs:
+
+            without_tag = re.match("\$\{?([^}'\s]+)\}?", inparam).group(1)
+            without_tag = without_tag.split(".value")[0]
+
+            if len(without_tag.split('.')) > 1:
+                continue
+
+            try:
+                left, right = inparam.split(without_tag)
+                resolved_new = ".".join(param_map[without_tag])
+                
+                old_text = inparam
+                new_text = left + resolved_new + right
+                
+                full_text = full_text.replace(old_text, new_text, 10)
+                
+                print(old_text, "---", new_text)
+            except KeyError:
+                print("cannot find", without_tag)
+
+
+        print(full_text)
